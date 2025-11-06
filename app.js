32aaab6ac51bfbcdb04a673c086a208b246b8d1f
Vegad Het <vegadhet279@gmail.com>
2025-11-06 22:46:27 +0530

    change app.js as per serializing fails

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV != "production");{
+if (process.env.NODE_ENV !== "production") {
   require('dotenv').config();
 }
 
@@ -8,7 +8,6 @@ const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const MongoStore = require('connect-mongo');
 const flash = require("connect-flash");
@@ -16,90 +15,80 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
-const listingRouter = require("./routes/listing.js")
-const reviewRouter = require("./routes/review.js")
-const userRouter = require("./routes/user.js")
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
-
-//const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+// MongoDB connection
 const dbUrl = process.env.ATLASDB_URL;
 
-main()
-  .then(() => {
-    console.log("connected to DB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+mongoose.connect(dbUrl)
+  .then(() => console.log("Connected to DB"))
+  .catch(err => console.log(err));
 
-async function main() {
-  await mongoose.connect(dbUrl);
-}
-
+app.engine('ejs', ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine('ejs', ejsMate);
-app.use(express.static(path.join(__dirname,"/public")));
+app.use(express.static(path.join(__dirname, "public")));
 
+// Session store
 const store = MongoStore.create({
   mongoUrl: dbUrl,
   crypto: {
     secret: process.env.SECRET,
   },
-  touchAfter: 24 * 2600,
+  touchAfter: 24 * 3600, // seconds
 });
 
-store.on("error", () => {
-  console.log("ERROR in MONGO SESSION STORE", err);
+store.on("error", (err) => {
+  console.log("MONGO SESSION STORE ERROR", err);
 });
 
 const sessionOptions = {
+  store,
   secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
+    httpOnly: true,
     expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
     maxAge: 7 * 24 * 60 * 60 * 1000,
-    httpOnly: true
   },
 };
 
-app.get("/", (req, res) => {
-  res.send("Hi, I am root");
-});
-
-
 app.use(session(sessionOptions));
-app.use(flash())
+app.use(flash());
 
+// Passport config
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
 
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Flash & current user middleware (after passport.session)
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
-  res.locals.currUser = req.user;
+  res.locals.currUser = req.user; // <-- now safe
   next();
-})
-
-app.use("/listings", listingRouter)
-app.use("/listings/:id/reviews", reviewRouter)
-app.use("/", userRouter);
-
-
-app.use((err, req, res, next) => {
-  let{ statusCode=500, message= "Something went wrong"} = err;
-  res.status(statusCode).render("error.ejs", {message});
-  //res.status(statusCode).send(message);
 });
 
-// app.all("*", (req, res, next) =>{
-//   next(new ExpressError(404, "Page Not Found"));
-// });
+// Routes
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
+// Error handler
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message = "Something went wrong" } = err;
+  res.status(statusCode).render("error.ejs", { message });
+});
+
+// Server
 app.listen(8080, () => {
-  console.log("server is listening to port 8080");
+  console.log("Server listening on port 8080");
 });
